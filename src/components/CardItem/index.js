6b80c4141338ae9c446e77bc37875e7eae05bf9b
nagamni <nagamani1490@gamail.com>
2023-02-07 12:37:15 +0000

    commit message

---
+++ src/components/CardItem/index.js
@@ -0,0 +1,18 @@
+// Write your code here.
+import  './index.css'
+
+const CardItem=(props)=>{
+ const{cardDetail , key}=props
+    { title, description, imgUrl,className }=cardDetail
+}
+   
+return(
+    <li className={`${className}'>
+        <div>
+            <h1 className="head">{title}</h1>
+            <p className="para">{description}</p>
+            <img src={imgUrl} className="img1"/>>
+        </div>
+    </li>
+
+)
